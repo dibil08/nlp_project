9d0925ddd42c13e3f4d967dc3bc597331753ab00
JanPelicon <jp1467@student.uni-lj.si>
2022-04-03 18:38:07 +0200

    Repo redesign and cleanup

---
+++ karst/README.txt
@@ -0,0 +1,6 @@
+Download from:
+https://drive.google.com/drive/folders/17LF5gKGX-bgBL9NAa037pum4-i7gpqhm
+
+Rename to:
+karst_annotated_definitions.zip
+karst_corpora.zip
--- main.py
+++
@@ -1,5 +0,0 @@
-import gdown
-
-url = 'https://drive.google.com/drive/folders/1GV3F8oqn3N-hfuHpAigjBRHYBtEIH5xR'
-output = 'data'
-gdown.download(url, output, quiet=False)
